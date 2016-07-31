bb0df47e217d581e0dd4483687b1e933381d8fee
javascript-structure <brassski.val@yandex.ru>
2016-07-31 17:45:41 +0300

    new files

---
+++ jquery.bxslider/now.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider-arrey').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider-date').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider-documetn').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+
+$(document).ready(function(){
+  $('.bxslider-function').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider-if').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+$(document).ready(function(){
+  $('.bxslider-math').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+
+
+
+$(document).ready(function(){
+  $('.bxslider-while').bxSlider({
+	  mode: 'fade'
+  });
+});
+
+
+
+
+$(document).ready(function(){
+  $('.bxslider-object').bxSlider({
+	  mode: 'fade'
+  });
+});
